c8b832c107a6f4e58d9233dd032807d10b3bd86b
hj-66 <a01088200534@gmail.com>
2026-05-08 11:11:23 +0900

    dev와 test 프로파일 분리

---
+++ src/main/java/com/mysite/sbb/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.mysite.sbb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "SBB";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "안녕하세요 ^ ^";
+    }
+}
